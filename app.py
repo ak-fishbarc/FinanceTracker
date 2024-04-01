7b5d1ceb504eeda78ef368f4d86087374a2de015
ak-fishbarc <rybarczyk.ak@gmail.com>
2024-04-01 20:56:46 +0100

    Added flask_login to login

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, flash
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from config import Config
+import sqlalchemy
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager, current_user, login_user
 from forms import LoginForm
-
+import models
 
 app = Flask(__name__)
 app.config.from_object(Config)
 limiter = Limiter(key_func=get_remote_address, app=app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login_manager = LoginManager(app)
 
 
 @app.route('/')
@@ -22,8 +25,15 @@ def home_page():
 @app.route('/login', methods=['POST', 'GET'])
 @limiter.limit("5 per minute")
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home_page'))
     form = LoginForm()
     if form.validate_on_submit():
+        user = db.session.scalar(sqlalchemy.select(models.User).where(models.User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user)
         return redirect(url_for('home_page'))
     return render_template("login.html", form=form)
 
@@ -38,5 +48,13 @@ def rate_limit_handler(e):
     return "Rate limit exceeded!"
 
 
+@login_manager.user_loader
+def load_user(user):
+    return db.session.get(models.User, int(user))
+
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
