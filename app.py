ece16071375813c083af4f1e56db2ef743f37161
ak-fishbarc <rybarczyk.ak@gmail.com>
2024-04-02 20:34:34 +0100

    Added error handlers

--- app.py
+++ app.py
@@ -66,8 +66,20 @@ def registration():
 
 
 @app.errorhandler(429)
-def rate_limit_handler(e):
-    return "Rate limit exceeded!"
+def rate_limit_handler(error):
+    return render_template('429.html'), 429
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    with app.app_context():
+        db.session.rollback()
+        return render_template('500.html'), 500
 
 
 @login_manager.user_loader
