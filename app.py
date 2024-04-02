f8cfd78d8f60fab0de3418e52448e6809f728b42
ak-fishbarc <rybarczyk.ak@gmail.com>
2024-04-02 20:23:42 +0100

    Added registration form and registration page.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, flash
+from flask import Flask, render_template, redirect, url_for, flash, request
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from config import Config
@@ -6,8 +6,9 @@ import sqlalchemy
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager, current_user, login_user, logout_user
-from forms import LoginForm
+from urllib.parse import urlsplit
 import models
+import forms
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -15,9 +16,11 @@ limiter = Limiter(key_func=get_remote_address, app=app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 
 @app.route('/')
+@app.route('/home_page')
 def home_page():
     return render_template("home_page.html")
 
@@ -27,14 +30,17 @@ def home_page():
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('home_page'))
-    form = LoginForm()
+    form = forms.LoginForm()
     if form.validate_on_submit():
         user = db.session.scalar(sqlalchemy.select(models.User).where(models.User.username == form.username.data))
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user)
-        return redirect(url_for('home_page'))
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('home_page')
+        return redirect(next_page)
     return render_template("login.html", form=form)
 
 
@@ -45,8 +51,18 @@ def logout():
 
 
 @app.route('/register', methods=['POST', 'GET'])
-def register():
-    return "Register"
+def registration():
+    if current_user.is_authenticated:
+        return redirect(url_for('home_page'))
+    form = forms.RegistrationForm()
+    if form.validate_on_submit():
+        user = models.User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Registration was successful')
+        return redirect(url_for('login'))
+    return render_template('registration.html', form=form)
 
 
 @app.errorhandler(429)
--- forms.py
+++ forms.py
@@ -1,9 +1,34 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, SubmitField, EmailField
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+import sqlalchemy
+import app
 
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Sign In')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = EmailField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        with app.app.app_context():
+            user = app.db.session.scalar(sqlalchemy.select(app.models.User).where(
+                app.models.User.username == username.data))
+            if user is not None:
+                raise ValidationError('Invalid Username')
+
+    def validate_email(self, email):
+        with app.app.app_context():
+            user = app.db.session.scalar(sqlalchemy.select(app.models.User).where(
+                app.models.User.email == email.data))
+            if user is not None:
+                raise ValidationError('Invalid Email')
+
